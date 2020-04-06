cffcbdeb9e83490343b02b02438bb33e3648a1f6
Nikhil Tadikonda <nikhiltadikonda@gmail.com>
2020-04-06 19:11:20 +0530

    Pushing Files to Stage 1

---
+++ app/src/main/java/com/project/crop_prediction/ProfileActivity.java
@@ -0,0 +1,37 @@
+package com.project.crop_prediction;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private TextView username;
+    private ImageView profileDP;
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        username=findViewById(R.id.profile_uname);
+        profileDP=findViewById(R.id.profile_dp);
+
+        firebaseAuth=FirebaseAuth.getInstance();
+
+        if(firebaseAuth.getCurrentUser()!=null){
+            FirebaseUser user=firebaseAuth.getCurrentUser();
+            username.setText(user.getDisplayName());
+            Glide.with(this).load(user.getPhotoUrl()).apply(RequestOptions.circleCropTransform()).into(profileDP);
+
+        }
+    }
+}
